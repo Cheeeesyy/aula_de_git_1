d3b5b090d1ab77cac4deb2d43a4c7e8ab5ef0bea
gabriel pedroso <gabrielpedroso321@gmail.com>
2022-06-11 11:42:28 -0300

    arquivos novos da aula de sábado

---
+++ mach1_logica_programação/fundamentos/bloco_agrupador.js
@@ -0,0 +1,17 @@
+{
+    console.log("bloco 1 - sentença 1");
+    console.log("bloco 1 - sentença 2");
+    console.log("bloco 1 - sentença 3");
+}
+
+{
+    {
+        {
+            {
+                console.log("bloco 2 - sentença 1");
+                console.log("bloco 2 - sentença 2");
+                console.log("bloco 2 - sentença 3");
+            }
+        }
+   }
+}
---
+++ mach1_logica_programação/fundamentos/inicio.js
@@ -0,0 +1,14 @@
+// Estou escrevendo um comentário de linha
+
+console.log('Olá mundo esse é o primeiro teste');
+console.log("Escrevendo uma nova sentença!");
+
+/*
+Estou
+escrevendo um comentario
+de multiplas linhas
+*/
+
+console.log("Ordem1");
+console.log("Ordem2");
+console.log("Ordem3");
